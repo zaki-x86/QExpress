c271da462be5dcd73af0316606300b52f9e8f194
zaki-x86 <mamadovzicx@gmail.com>
2024-02-07 23:54:14 +0200

    test: added rest of test files for project modules

--- test/router.unittests.cpp
+++ test/router.unittests.cpp
@@ -4,6 +4,31 @@
 #include "Request.h"
 #include "Response.h"
 
-TEST_CASE("Basic QExpress::Router unit tests") {
+TEST_SUITE_BEGIN("Router"
+                        * doctest::description("Basic Router unit tests"));
 
-}
+TEST_CASE("QExpress:Router::CTOR"){
+    using namespace QExpress;
+    Router router;
+    Router* routerPtr = &router;
+    CHECK(routerPtr != nullptr);
+
+    std::shared_ptr<Router> routerPtr2 = std::make_shared<Router>();
+    CHECK(routerPtr2 != nullptr);
+} // QExpress::Router::CTOR
+
+TEST_CASE("QExpress:Router::COPY"){} // QExpress::Router::COPY
+
+TEST_CASE("QExpress:Router::MOVE"){} // QExpress::Router::MOVE
+
+TEST_CASE("QExpress:Router::DTOR"){} // QExpress::Router::DTOR
+
+TEST_CASE("QExpress:Router::route"){} // QExpress::Router::route
+
+TEST_CASE("QExpress:Router::param"){} // QExpress::Router::param
+
+TEST_CASE("QExpress:Router::use"){} // QExpress::Router::use
+
+TEST_CASE("QExpress:Router::METHOD"){} // QExpress::Router::METHOD
+
+TEST_SUITE_END();
--- test/server.unittests.cpp
+++ test/server.unittests.cpp
@@ -5,17 +5,117 @@
 #include <memory>
 #include "doctest.h"
 
-TEST_CASE("Server can be constructed") {
-    INFO("Server can be constructed");
+TEST_SUITE_BEGIN("Server tests" 
+                    * doctest::description("Basic server unit tests"));
+
+TEST_CASE("QExpress::Server::CTOR") {
     char ** args = new char*;
     args[0] = "1";
     QExpress::Server server(1,args);
-    QExpress::Request req;
-    QExpress::Response res;
     server.get("/", [](QExpress::Request& req, QExpress::Response& res) {
         
     });
-}
+} // QExpress::Server::CTOR
+
+TEST_CASE("QExpress::Server::COPY") {} // QExpress::Server::COPY
+
+TEST_CASE("QExpress::Server::MOVE") {} // QExpress::Server::MOVE
+
+TEST_CASE("QExpress::Server::DTOR") {
+    /**
+     * @brief Constructs the server within a scope
+     * checks if all server resources are destroyed properly after going out-of-scope
+     */
+    
+} // QExpress::Server::DTOR
+
+TEST_CASE("QExpress:Server::listen") {
+    SUBCASE("Server can be started on specific port") {}
+
+    SUBCASE("Server can be started with a callback function") {}
+
+    SUBCASE("Server can be started with on a specified host address") {}
+
+} // QExpress:Server::listen
+
+TEST_CASE("QExpress:Server::createRouter") {
+
+} // QExpress:Server::createRouter
+
+TEST_CASE("QExpress:Server::router") {
+    
+} // QExpress:Server::router
+
+TEST_CASE("QExpress:Server::use"){
+    SUBCASE("Mounting functions without path"){
+        SUBCASE("Server can mount a middleware function") {}
+
+        SUBCASE("Server can mount a comma separated list of middleware functions") {}
+
+        SUBCASE("Server can mount an initalizer list of middleware functions") {}
+
+    } // Mounting functions without path
+    
+    SUBCASE("Mounting functions to specified path") {
+        SUBCASE("Server can mount a middleware function to specific path") {}
+
+        SUBCASE("Server can mount a comma separated list of middleware functions to specific path") {}
+
+        SUBCASE("Server can mount an initalizer list of middleware functions to specific path") {}
+    } // Mounting functions to specified path
+
+    SUBCASE("Mounting routers without path"){
+        SUBCASE("Server can mount a middleware router object") {}
+
+        SUBCASE("Server can mount a comma separated list of middleware routers") {}
+
+        SUBCASE("Server can mount an initalizer list of middleware router objects") {}
+    } // Mounting routers without path
+
+    SUBCASE("Mounting routers to specified path"){
+        SUBCASE("Server can mount a middleware router object to specific path") {}
+
+        SUBCASE("Server can mount a comma separated list of middleware routers to specific path") {}
+
+        SUBCASE("Server can mount an initalizer list of middleware router objects to specific path") {}
+    } // Mounting routers to specified path
+
+    SUBCASE("Mounting server subapps without path") {
+        SUBCASE("Server can mount a server subapp") {}
+
+        SUBCASE("Server can mount a comma separated list of server subapps") {}
+
+        SUBCASE("Server can mount an initalizer list of server subapps") {}
+    } // Mounting server subapps without path
+
+    SUBCASE("Mounting server subapps to specified path") {
+        SUBCASE("Server can mount a server subapp to specified path") {}
+
+        SUBCASE("Server can mount a comma separated list of server subapps to specified path") {}
+
+        SUBCASE("Server can mount an initalizer list of server subapps to specified path") {}
+    } // Mounting server subapps to specified path
+
+} // QExpress:Server::use
+
+
+TEST_CASE("QExpress:Server::METHOD"){
+    SUBCASE("Server can register a GET method") {}
+
+    SUBCASE("Server can register a POST method") {}
+
+    SUBCASE("Server can register a PUT method") {}
+
+    SUBCASE("Server can register a DELETE method") {}
+
+    SUBCASE("Server can register a ALL method") {}
+
+    SUBCASE("Server can register serveral functions to the specified method-path") {}
+
+    SUBCASE("Server can mount router(s) to the specified method-path") {}
+} // QExpress:Server::METHOD
+
+TEST_SUITE_END();
 
 // class TestServer: public QObject
 // {
